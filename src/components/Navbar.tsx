b068059f0f6779d2f59ca36d5c0ef094808efd44
Mac_Developer <araozangel842@gmail.com>
2022-04-30 20:16:49 +0300

    Styles and Light/Dark button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react';
-import { ThemeManager } from '../Context/Context';
 import '../Styles/Components/Navbar.scss';
+import ThemeToggle from './ThemeToggle';
 
-const Navbar = () => {
-  const { theme } = useContext(ThemeManager);
-  console.log('theme from Navbar : ', theme);
-  return <div className="navbar">Navbar</div>;
+const Navbar = (): JSX.Element => {
+  return (
+    <div className="navbar">
+      <div className="navbar-img">
+        <img
+          src="https://res.cloudinary.com/dqaerysgb/image/upload/v1651333819/Earth-Planet-PNG-Picture_btbdtv.png"
+          style={{ width: '10opx', height: '100px' }}
+          alt=""
+        />
+      </div>
+      <div className="emptyDiv"></div>
+      <div className="navbar-buttons">
+        <ThemeToggle />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
