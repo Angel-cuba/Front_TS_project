c425062b84cbecdafac7b056943f971b3703b5bb
Mac_Developer <araozangel842@gmail.com>
2022-04-30 20:17:49 +0300

    Using Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ThemeManager } from './Context/Context';
 import Routes from './Routes/Routes';
 
 export default function App() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState<string>('light');
 
   return (
     <ThemeManager.Provider value={{ theme, setTheme }}>
